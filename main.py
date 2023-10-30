d069d048e69b1daf9d19483c319532c3c94b1e74
pawlocat <7urt1e7777@gmail.com>
2023-10-30 23:03:32 +0800

    Background

--- main.py
+++ main.py
@@ -8,15 +8,33 @@ pygame.init()
 
 pygame.display.set_caption("Playing")
 
-BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1000, 800
 FPS = 60
 PLAYER_VEL = 5 #player Speed
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def get_background(name): #load bg from assets
+  image = pygame.image.load(join("assets", "Background", name))
+  _, _, width, height = image.get_rect() 
+  tiles = [] # create an array
+  
+  for i in range(WIDTH // width + 1): #check how many tiles need to fill screen
+    for j in range(HEIGHT // height + 1):
+      pos = (i * width, j * height)
+      tiles.append(pos)
+      
+  return tiles, image
+
+def draw(window, background, bg_image):
+  for tile in background:
+    window.blit(bg_image, tile)
+    
+  pygame.display.update()
+
 def main(window):
   clock = pygame.time.Clock()
+  background, bg_image = get_background("Yellow.png")
   
   run = True
   while run:
@@ -27,6 +45,7 @@ def main(window):
         run = False
         break
       
+    draw(window, background, bg_image)    
   pygame.quit()
   quit()
 
