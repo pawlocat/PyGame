c96a8f72335558fff66a805206a16d3f60564065
pawlocat <7urt1e7777@gmail.com>
2023-10-30 23:18:28 +0800

    create player , moving

--- main.py
+++ main.py
@@ -10,10 +10,43 @@ pygame.display.set_caption("Playing")
 
 WIDTH, HEIGHT = 1000, 800
 FPS = 60
-PLAYER_VEL = 5 #player Speed
+PLAYER_VEL = 5 #player moving speed
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+class Player(pygame.sprite.Sprite):
+  COLOR = (255, 0, 0)
+  
+  def __init__(self, x, y, width, height):
+    self.rect = pygame.Rect(x, y, width, height)
+    self.x_vel = 0 #player moving speed
+    self.y_vel = 0 #player moving speed
+    self.mask = None
+    self.direction = "left"
+    self.animation_count = 0
+  
+  def move(self, dx, dy):
+    self.rect.x += dx
+    self.rect.y += dy
+    
+  def move_left(self, vel):
+    self.x_vel = -vel
+    if self.direction != "left":
+      self.direction = "left"
+      self.animation_count = 0
+      
+  def move_right(self, vel):
+    self.x_vel = vel  
+    if self.direction != "right":
+      self.direction = "right"
+      self.animation_count = 0
+  
+  def loop(self, fps):
+    self.move(self.x_vel, self.y_vel)
+  
+  def draw(self, win):
+    pygame.draw.rect(win, self.COLOR, self.rect)
+      
 def get_background(name): #load bg from assets
   image = pygame.image.load(join("assets", "Background", name))
   _, _, width, height = image.get_rect() 
@@ -26,16 +59,29 @@ def get_background(name): #load bg from assets
       
   return tiles, image
 
-def draw(window, background, bg_image):
+def draw(window, background, bg_image, player):
   for tile in background:
     window.blit(bg_image, tile)
-    
+  
+  player.draw(window)
+  
   pygame.display.update()
+
+def handle_move(player):
+  keys = pygame.key.get_pressed()
+  
+  player.x_vel = 0
+  
+  if keys[pygame.K_LEFT]:
+    player.move_left(PLAYER_VEL)
+  if keys[pygame.K_RIGHT]:
+    player.move_right(PLAYER_VEL)
 
 def main(window):
   clock = pygame.time.Clock()
   background, bg_image = get_background("Yellow.png")
   
+  player = Player(100, 100, 50, 50)
   run = True
   while run:
     clock.tick(FPS) #let game run 60FPS
@@ -45,7 +91,9 @@ def main(window):
         run = False
         break
       
-    draw(window, background, bg_image)    
+    player.loop(FPS)
+    handle_move(player)  
+    draw(window, background, bg_image, player)    
   pygame.quit()
   quit()
 
