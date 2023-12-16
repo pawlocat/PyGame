77143caef459285a4c7ed247068bf698afc4561e
pawlocat <7urt1e7777@gmail.com>
2023-12-17 00:53:03 +0800

    floor

--- main.py
+++ main.py
@@ -42,12 +42,21 @@ def load_sprite_sheets(dirl, dir2, width, height, direction=False):
       
   return all_sprites
 
+def get_block(size):
+  path = join("assets", "Terrain", "Terrain.png")
+  image = pygame.image.load(path).convert_alpha()
+  surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
+  rect = pygame.Rect(96, 0, size, size)
+  surface.blit(image, (0, 0), rect)
+  return pygame.transform.scale2x(surface)
+
 class Player(pygame.sprite.Sprite):
   COLOR = (255, 0, 0)
   GRAVITY = 1
   SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True)
   ANIMATION_DELAY = 3
   def __init__(self, x, y, width, height):
+    super().__init__()
     self.rect = pygame.Rect(x, y, width, height)
     self.x_vel = 0 #player moving speed
     self.y_vel = 0 #player moving speed
@@ -89,11 +98,34 @@ class Player(pygame.sprite.Sprite):
     sprite_index = (self.animation_count // self.ANIMATION_DELAY) % len(sprites)
     self.sprite = sprites[sprite_index]
     self.animation_count += 1
-  
+    
+  def update(self):
+    self.rect = self.sprite.get(rect(topLeft=(self.rect.x, self.rect.y)))
+    self.mask = pygame.mask,from_surface(self.sprite)
+    
   def draw(self, win):
     #self.sprite = self.SPRITES["idle_" + self.direction][0] #52 25
     win.blit(self.sprite, (self.rect.x, self.rect.y))
       
+class Object(pygame.sprite.Sprite):
+  def __init__(self, x, y, width, height, name=None):
+    super().__init__()
+    self.rect = pygame.Rect(x, y, width, height)
+    self.image = pygame.Surface((width, height), pygame.SRCALPHA)
+    self.width = width
+    self.height = height
+    self.name = name
+  
+  def draw(self, win):
+    win.blit(self.image, (self.rect.x, self.rect.y))
+    
+class Block(Object):
+  def __init__(self, x, y, size):
+      super().__init__(x, y, size, size)
+      block = get_block(size)
+      self.image.blit(block, (0, 0))
+      self.mask = pygame.mask.from_surface(self.image)
+    
 def get_background(name): #load bg from assets
   image = pygame.image.load(join("assets", "Background", name))
   _, _, width, height = image.get_rect() 
@@ -106,10 +138,13 @@ def get_background(name): #load bg from assets
       
   return tiles, image
 
-def draw(window, background, bg_image, player):
+def draw(window, background, bg_image, player, objects):
   for tile in background:
     window.blit(bg_image, tile)
   
+  for obj in objects:
+    obj.draw(window)
+    
   player.draw(window)
   
   pygame.display.update()
@@ -128,7 +163,11 @@ def main(window):
   clock = pygame.time.Clock()
   background, bg_image = get_background("Yellow.png")
   
+  block_size = 96
+  
   player = Player(100, 100, 50, 50)
+  floor = [Block(i * block_size, HEIGHT - block_size, block_size) 
+          for i in range(-WIDTH // block_size, (WIDTH * 2) // block_size)]
   run = True
   while run:
     clock.tick(FPS) #let game run 60FPS
@@ -140,7 +179,7 @@ def main(window):
       
     player.loop(FPS) #60FPS run this def
     handle_move(player)  
-    draw(window, background, bg_image, player)    
+    draw(window, background, bg_image, player, floor)    
   pygame.quit()
   quit()
 
